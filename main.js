e5745c5bb48d2aa11350136fc9122d9350f0235c
Lao-9R-ay2526 <lao.lil@obmontessori.edu.ph>
2026-05-08 10:08:44 +0800

    Refactor determinepass function for clarity

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ function determinepass() {
   
     if (userEntry.length < 5) {
         feedback.innerHTML = "Error: Password must be 5+ characters.";
-        feedback.style.color = "red";
+        
     } else {
         feedback.innerHTML = "Success! Password accepted.";
-        feedback.style.color = "green";
+   
         window.alert("Entered password: " + userEntry);
     }
 }
